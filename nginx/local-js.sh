4b5d1afd0405ec3e7874498b05abde9c50d1fcb3
Ricardo Ferreira <riferrei@riferrei.com>
2026-03-07 22:00:19 -0500

    Fix Codespaces frontend routing and port forwarding

--- nginx/local-js.sh
+++ nginx/local-js.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-cat << EOF > /usr/share/nginx/html/scripts/apis.js
-const searchAPI = "${SEARCH_API}"
-EOF
+set -eu
 
-# Start Nginx
-exec nginx -g 'daemon off;'
+SEARCH_API_URL="${SEARCH_API:-http://localhost:8081/search}"
+
+# In Codespaces, point the frontend to the forwarded backend URL.
+if [ -n "${CODESPACE_NAME:-}" ] && [ -n "${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN:-}" ]; then
+  SEARCH_API_URL="https://${CODESPACE_NAME}-8081.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}/search"
+fi
+
+cat << EOF > /usr/share/nginx/html/scripts/apis.js
+const searchAPI = "${SEARCH_API_URL}"
+EOF
