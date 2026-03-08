9acb6de9d593fd13f00a52f8fd7972d37e2300d2
Ricardo Ferreira <riferrei@riferrei.com>
2026-03-07 22:08:58 -0500

    Route frontend search through nginx in dev containers

--- nginx/local-js.sh
+++ nginx/local-js.sh
@@ -2,11 +2,15 @@
 
 set -eu
 
-SEARCH_API_URL="${SEARCH_API:-http://localhost:8081/search}"
+SEARCH_API_URL="${SEARCH_API:-}"
 
-# In Codespaces, point the frontend to the forwarded backend URL.
-if [ -n "${CODESPACE_NAME:-}" ] && [ -n "${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN:-}" ]; then
-  SEARCH_API_URL="https://${CODESPACE_NAME}-8081.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}/search"
+if [ -z "${SEARCH_API_URL}" ]; then
+  SEARCH_API_URL="http://localhost:8081/search"
+
+  # In Codespaces, point the frontend to the forwarded backend URL.
+  if [ -n "${CODESPACE_NAME:-}" ] && [ -n "${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN:-}" ]; then
+    SEARCH_API_URL="https://${CODESPACE_NAME}-8081.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}/search"
+  fi
 fi
 
 cat << EOF > /usr/share/nginx/html/scripts/apis.js
