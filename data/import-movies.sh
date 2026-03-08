4ced3ef9fe275622e034728f874236d45413dc72
Ricardo Ferreira <riferrei@riferrei.com>
2026-03-07 20:45:57 -0500

    Implemented lab 2 changes

--- data/import-movies.sh
+++ data/import-movies.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-riot file-import \
-    --var counter="new java.lang.Integer(1)" \
-    --proc id="#counter++" \
-    --proc plot="info.plot" \
-    --proc releaseDate="info.release_date" \
-    --proc rating="info.rating" \
-    --proc actors="info.actors != null ? remove('info').actors : null" \
-    movies.json json.set --keyspace movie --key id
+# Implement the movies import using RIOT
